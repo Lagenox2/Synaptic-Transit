f272616ac86f75381f5b6600acbb73cf12184084
MechanicProfi <probogprofi@gmail.com>
2026-01-19 18:17:46 +0300

    1

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import pygame
 from screeninfo import get_monitors
 
-
-for m in get_monitors():
-    print(f"Монитор: {m.name}, Разрешение: {m.width}x{m.height}, Рабочая область: {m.width}x{m.height}")
-    WIDTH = m.width
-    HEIGHT = m.height
-
-pygame.init()
-pygame.mixer.init()  # для звука
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
-pygame.display.set_caption("Synaptic Transit")
-# clock = pygame.time.Clock()
-running = True
-
 def main():
+    for m in get_monitors():
+        print(f"Монитор: {m.name}, Разрешение: {m.width}x{m.height}, Рабочая область: {m.width}x{m.height}")
+        WIDTH = m.width
+        HEIGHT = m.height
+
+    pygame.init()
+    pygame.mixer.init()  # для звука
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+    pygame.display.set_caption("Synaptic Transit")
+    # clock = pygame.time.Clock()
+    running = True
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
