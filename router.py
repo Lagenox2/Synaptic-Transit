5b19eda06bc2a480f946ed1ef04a9c26704ea1bb
Lagenox2 <lagenox2@gmail.com>
2026-01-20 15:20:12 +0300

    НЕ УБИРАЙ ОТСТАЛЫЙ

--- router.py
+++ router.py
@@ -1,6 +1,11 @@
 import random
 
 class Router:
+    name = ''
+    connections = []
+    maximum_recieving = 0
+    recieving_now = 0
+    
     def __init__(self, name, connections):
         self.name = name
         self.connections = connections
